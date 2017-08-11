da3d21a5b678167a402583b49358ad5b65e6dfca
dweb1 <david.p.weber@gmail.com>
2017-08-11 17:23:50 -0400

    Added styled-components and styling to nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
-
+import styled from 'styled-components';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 // import Player from "./components/Player";
 import Team from "./components/Team";
 import Search from "./components/Search";
+
+const NavBar = styled.div`
+height: 30px;
+background-color: green;
+color: white;
+text-decoration: none;
+display: flex;
+justify-content: flex-end;
+align-items: center;
+a {
+  padding: 0 15px;
+  color: white;
+  text-decoration: none;
+ &:hover {
+   text-decoration: underline;
+ }
+}
+`;
 
 class App extends Component {
 
@@ -12,10 +30,10 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <div>
+          <NavBar>
             <Link to="/">Home</Link>
             <Link to="#">Profile</Link>
-          </div>
+          </NavBar>
           <div>
             <Route exact path="/" component={Home} />
             {/* <Route path="/player/:playerId" component={Player} /> */}
